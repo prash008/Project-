f80c4bb027457b9acfe520d4c0fc4752ca8f09e1
prash008 <prashanth.lg.94@gmail.com>
2017-01-24 12:07:35 +0530

    modified user list part

--- userlist.c
+++ userlist.c
@@ -1,10 +1,16 @@
+#include "status.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <pthread.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/scrnsaver.h>
 
 #define CLADDR_LEN 100
 #define ipa 16
@@ -15,14 +21,14 @@
 #define buff 200
 #define PORT 4444
 
-int i,index=0;
-char *id[100];
-char *ip[100],*buf1[ipa+1];
+static int y=0;
 
  struct ip_list
   {
    char usr[user];
    char ip[ipa]; 
+   int busy;
+   int away;
    struct ip_list *next;
   };
 
@@ -30,13 +36,20 @@ char *ip[100],*buf1[ipa+1];
   struct ip_list *node;
   struct ip_list *newnode;
 
+struct info
+ {
+  char username[50];
+  int busy;
+ int away;
+ };
+ struct info myinfo;
 
  void* sender(void *ptr)
   {
    int sockfd, status, buflen, sinlen,ret;
-   char buffer[MAXBUF+1];
    struct sockaddr_in sock_in;
-   int yes = 1,interval=10,seconds_to_sleep,duration,elapsed;
+   int yes = 1,interval=10,seconds_to_sleep,duration,elapsed,i;
+   
   
    sinlen = sizeof(struct sockaddr_in);
    memset(&sock_in, 0, sinlen);
@@ -48,30 +61,27 @@ char *ip[100],*buf1[ipa+1];
    sock_in.sin_port = htons(44444); 
    sock_in.sin_family = AF_INET;
    status = bind(sockfd, (struct sockaddr *)&sock_in, sinlen);
-   status = setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &yes, sizeof(int) );
-        
-
-   memset(buffer, 0, BUF_SIZE);
- 
-
-    if(ret) 
-     {
-      printf("ERROR: Return Code from pthread_create() is %d\n", ret);
-      exit(1);
-     }
-
-                
-      for(i=0;i<=3;i++) 
+   status = setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &yes, sizeof(int) );            
+   
+   for(;;)
        {
-        
-         sprintf(buffer, "umesh");
-         ret = sendto(sockfd, buffer, buflen, 0, (struct sockaddr *)&sock_in, sinlen);  
+        myinfo.busy=getbusystatus();
+         if(y>=10)
+         {
+         myinfo.away=1;
+         }
+          else 
+          {
+         myinfo.away=0;
+          } 
+        // printf("%d",myinfo.away);
+         ret = sendto(sockfd, &myinfo, sizeof(struct info), 0, (struct sockaddr *)&sock_in, sinlen);  
          if (ret < 0) 
           {  
-            printf("Error sending data!\n\t-%s\n", buffer);  
+            printf("Error sending data!\n\t-%s\n",&myinfo);  
           }
          sleep(1);
-          }
+       }
     // close(sockfd);
    }
  
@@ -79,14 +89,14 @@ char *ip[100],*buf1[ipa+1];
    void *receiver(void *ptr)
     {
      struct sockaddr_in si_me, si_other;
-     int s, i, slen = sizeof(si_other) , recv_len,ret,k;
-     char buf[BUFLEN+1];
-    
+     int s, i, slen = sizeof(si_other) , recv_len,ret,k,x,z;
+     struct info userinfo;
+     
      if((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
        {
         printf("error in socket");
        }
-     
+   
     memset((char *) &si_me, 0, sizeof(si_me));
      
     si_me.sin_family = AF_INET;
@@ -97,53 +107,120 @@ char *ip[100],*buf1[ipa+1];
     if( bind(s,(struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
      {
       printf("error in creating bind");
+      exit(0);
      }
+
+  
       
-      for(i=0;i<=3;i++)
+     for(;;)
        {
         fflush(stdout);
        
-         if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
-          {
-            printf("error in recvfrom()");
-          }
-         
+        if ((recv_len = recvfrom(s, &userinfo,sizeof(struct info), 0, (struct sockaddr *) &si_other, &slen)) == -1)
+         {
+          printf("error in recvfrom()");
+         }
+      
           if(head==NULL)
             {
              head=(struct ip_list *)malloc(sizeof(struct ip_list));
-             strcpy(head->usr,buf);
+             strcpy(head->usr,userinfo.username);
              strcpy(head->ip,inet_ntoa(si_other.sin_addr));
+             head->busy=userinfo.busy;
+             head->away=userinfo.away;
              head->next=NULL;
              node=head;
-             userlist();
              continue;
-             } 
+            } 
            else
              {
               newnode=(struct ip_list *)malloc(sizeof(struct ip_list));
-              strcpy(newnode->usr,buf);
+              strcpy(newnode->usr,userinfo.username);
               strcpy(newnode->ip,inet_ntoa(si_other.sin_addr));
-              k=findipinlist(newnode->ip);
+              newnode->busy=userinfo.busy;
+              newnode->away=userinfo.away;
+              k=findipinlist(newnode->usr);
+              x=changebusy(newnode->busy,newnode->ip);
+              z=changeaway(newnode->away,newnode->ip);
               newnode->next=NULL;
              } 
+           
            if(k==-1)
             {
-          node->next=newnode;
-          node=newnode;
-          userlist();
-            }       
+             node->next=newnode;
+             node=newnode;
+            } 
+           
+          
+           
+           if(z==0)
+            {
+              node->away=newnode->away;
+            }
+                  
        } 
     close(s);
     return 0;
    }
  
-  int findipinlist(char *ip)
+int getUserListCount()
+ {
+    int i =0;
+    node=head;
+    while(node!=NULL)
+    { 
+       i++;
+       node=node->next;
+      
+    }
+   return i; 
+  
+ }
+
+ char *getUserIP(int index)
+  {
+   int i=1;
+   node=head;
+   while(node!=NULL)
+    { 
+	
+ 	if(i!=index)
+          {
+ 	    node=node->next;
+            i++;
+          }
+         else
+          return node->ip;
+          
+     }
+   return -1;
+  }
+
+ char *getUserID(int index)
+  { 
+   int i=1;
+   node=head;
+   while(node!=NULL)
+     {
+      if(i!=index)
+        {
+         node=node->next;
+         i++;
+        }
+       else
+         return node->usr;
+         
+       }
+     return -1;
+    }     
+
+ int findipinlist(char *ip)
    {
     node=head;
     while(node!=NULL)
     { 
      
-     if (strcmp(node->ip,newnode->ip))
+     if (strcmp(node->ip ,newnode->ip))
      {
 	if(node->next)
 	 node=node->next;
@@ -155,50 +232,138 @@ char *ip[100],*buf1[ipa+1];
    }
    return -1; 
   }
-
-int userlist()
-{   
-    
-    id[index]=node->usr;
-    ip[index]=node->ip;
-    index++;
-    //list();
-}
-int list1()
-	{
-         pthread_t thread_reader,thread_writer,thread_reader1,thread_writer1;
-         int iret1,iret2;
- 	 char *message1="thread_reader";
- 	 char *message2="thread_writer";
-         int a,l;
-         iret1=pthread_create(&thread_writer,NULL,receiver,(void *)message2);
-         iret2=pthread_create(&thread_reader,NULL,sender,(void *)message1);
- 	 pthread_join(thread_writer,NULL);
- 	 pthread_join(thread_reader,NULL); 
-         }
-
-int list()
-{
-for(i=0;i<=3;i++) 
-{
- if (id[i]==NULL)
-  break;
- printf("%d.%s\n",i,id[i]);
- printf("  %s\n",ip[i]);
-}
-}
-
-int main()
+ int *awaystatus(int index)
+  { 
+   int i=1;
+   node=head;
+   while(node!=NULL)
      {
-        int a,l;
-        list1();
-        printf("enter 1 to display list");
-        scanf("%d",&l);
-         switch (l)
+      if(i!=index)
+        {
+         node=node->next;
+         i++;
+        }
+       else
+         return node->away;;
+         
+       }
+     return -1;
+    } 
+ char *getuserstatus(int index)
+  {
+    int i=1;
+   node=head;
+   while(node!=NULL)
+     {
+      if(i!=index)
+        {
+         node=node->next;
+         i++;
+        }
+       else
+         return node->busy;
+      }
+     return -1;
+   }
+  
+ int changebusy(int *busy,char *ip)
+   { 
+    
+    node=head;
+    
+    while(node!=NULL)
+    { 
+      while(strcmp(node->ip,newnode->ip))
+          {
+             if(node->next)
+             {
+             node=node->next;
+             }	
+             else
+             return-1;
+          }
+       
+            if(node->busy!=newnode->busy)
+               {
+                node->busy=newnode->busy;
+               }   
+              return 0; 
+           }
+   return -1; 
+  }
+int changeaway(int *away,char *ip)
+   { 
+    
+    node=head;
+    
+    while(node!=NULL)
+    { 
+      while (strcmp(node->ip,newnode->ip))
+          {
+             if(node->next)
+             {
+             node=node->next;
+             }	
+             else
+             return-1;
+          }
+       if(node->away!=newnode->away)
+              {
+              node->away=newnode->away;
+              return 0;    
+              }  
+          return 0;
+     }
+   return -1; 
+  }
+ int GetIdleTime () 
+      {
+        time_t idle_time;
+        static XScreenSaverInfo *mit_info;
+        Display *display;
+        int screen;
+        mit_info = XScreenSaverAllocInfo();
+        if((display=XOpenDisplay(NULL)) == NULL) { return(-1); }
+        screen = DefaultScreen(display);
+        XScreenSaverQueryInfo(display, RootWindow(display,screen), mit_info);
+        idle_time = (mit_info->idle) / 1000 ;
+        XFree(mit_info);
+        XCloseDisplay(display);
+        return idle_time;
+      }
+
+     void *idle(void *ptr)
+       {
+          while(1)
+          {
+          sleep(1);
+          if(GetIdleTime()>0)
            {
-           case 1:list();
-                   break;
-           default :
-           printf("Invalid option\n" );
-            }
+            y++;
+           } 
+          else
+            y=0;
+          }
+       }
+  
+   int main(int argc,char *argv[])
+     {
+      if(argc<2)
+       {
+        printf(" enter username\n");
+        exit(0);
+       }
+      
+      strcpy(myinfo.username,argv[1]);
+      //myinfo.busy=0;                          
+      pthread_t thread_reader,thread_writer,thread_idletime;
+      int iret1,iret2,iret3;
+      char *message1="thread_reader";
+      char *message2="thread_writer";
+      char *message3="thread_idletime";
+      iret1=pthread_create(&thread_reader,NULL,receiver,(void *)message2);
+      iret2=pthread_create(&thread_writer,NULL,sender,(void *)message1);
+      iret3=pthread_create(&thread_idletime,NULL,idle,(void *)message3); 
+        
+      list1();
      }
